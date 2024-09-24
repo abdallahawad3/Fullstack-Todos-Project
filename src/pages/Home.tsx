96cd8831dc0b5ae504a839d1af8136d0318978ff
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-09-24 16:11:46 +0300

    Refactor useAuthenticationQuery hook to return correct data
    Fixes an issue in the useAuthenticationQuery hook where the returned data was not being correctly extracted from the API response. The hook now returns the 'data' property from the response instead of 'data.todos'.
    
    Also, add new functionality to the Home page
    
    - Added a modal for adding new todos
    - Implemented the logic for adding new todos, including making a POST request to the API with the new todo data
    - Updated the UI to display the new todos after they are added
    
    These changes improve the overall functionality and user experience of the Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,71 @@
-import { useState } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import Todo from "../components/Todo";
 import Button from "../components/ui/Button";
 import useAuthenticationQuery from "../hooks/useAuthenticationQuery";
 import type { ITodo } from "../interfaces";
 import TodoSkeleton from "../components/TodoSkeleton";
+import axiosInstance from "../config/axios.config";
+import Modal from "../components/ui/Modal";
+import MyTextarea from "../components/ui/Textarea";
+import Input from "../components/ui/Input";
 
 // LocalStorage..🛅
 const userDataString = localStorage.getItem("user");
 
 const HomePage = () => {
   // States..🗽
+  const [newTodo, setNewTodo] = useState({ title: "", description: "" });
+  const [isAddTodoModal, setIsAddTodoModal] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<ITodo>({ title: "", description: "", documentId: "" });
-  const { jwt } = userDataString ? JSON.parse(userDataString) : null;
+  const userData = userDataString ? JSON.parse(userDataString) : null;
   const { isLoading, data } = useAuthenticationQuery({
     queryKey: ["todos", editTodo.title],
-    url: "/users/me?populate=todos",
+    url: "/todos",
     config: {
       headers: {
-        Authorization: `Bearer ${jwt}`,
+        Authorization: `Bearer ${userData.jwt}`,
         "Content-Type": "Application/json",
       },
     },
   });
+
+  const onCloseAddTodoModal = () => {
+    setIsAddTodoModal(false);
+  };
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setNewTodo((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { status } = await axiosInstance.post(
+        "/todos",
+        {
+          data: {
+            title: newTodo.title,
+            description: newTodo.description,
+            // user: [userData.user.id],
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${userData.jwt}`,
+            "Content-Type": "Application/json",
+          },
+        }
+      );
+      if (status == 201) {
+        setIsAddTodoModal(false);
+        location.reload();
+        setNewTodo({ title: "", description: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // Handlers....🫱🏼‍🫲🏼
 
@@ -29,7 +73,7 @@ const HomePage = () => {
 
   if (isLoading) <TodoSkeleton />;
   const RenderTodo =
-    data &&
+    data?.length &&
     data.map((ele, idx) => (
       <Todo setEditTodo={setEditTodo} idx={idx + 1} todo={ele} key={ele.documentId} />
     ));
@@ -38,10 +82,34 @@ const HomePage = () => {
     <section className="mt-20 flex items-center justify-center">
       <div className="w-[100vw] space-y-3">
         <div className="flex justify-center">
-          <Button>ADD TODO</Button>
+          <Button
+            onClick={() => {
+              setIsAddTodoModal(true);
+            }}>
+            ADD TODO
+          </Button>
         </div>
         <div className="space-y-3">{data?.length ? RenderTodo : <h1> Not Todos Yet.!😅</h1>}</div>
       </div>
+      {/* Add Todo Modal */}
+      <Modal isOpen={isAddTodoModal} setOpenAndClose={onCloseAddTodoModal} title="ADD NEW TODO">
+        <form onSubmit={onSubmitHandler}>
+          <div>
+            <p className="text-sm/6 font-medium mb-1">Title</p>
+            <Input name="title" value={newTodo.title} onChange={onChangeHandler} />
+          </div>
+          <MyTextarea name="description" value={newTodo.description} onChange={onChangeHandler} />
+
+          <div className="flex items-center space-x-2 mt-2">
+            <Button isLoading={isLoading} type="submit" fullWidth variant={"default"}>
+              Add
+            </Button>
+            <Button onClick={onCloseAddTodoModal} fullWidth variant={"cancel"}>
+              cancel
+            </Button>
+          </div>
+        </form>
+      </Modal>
     </section>
   );
 };
